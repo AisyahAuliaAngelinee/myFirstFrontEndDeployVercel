677cb803668a4716a0d9f9d396479e856648a16e
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2023-11-07 12:53:08 +0700

    feat: showallcuisine

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ var swiper = new Swiper(".home-slider", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-        delay: 7500,
+        delay: 5500,
         disableOnInteraction: false,
     },
     pagination: {
