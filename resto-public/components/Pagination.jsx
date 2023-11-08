3e12df24012fb0265d97b1b6b25102be45ec17ab
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2023-11-08 20:02:53 +0700

    feat: revisi-public-page

--- resto-public/components/Pagination.jsx
+++ resto-public/components/Pagination.jsx
@@ -1,16 +1,20 @@
 const Pagination = () => {
+
     return (
-        <section class="pagination flex place-content-center">
-            <div class="flex flex-col w-[7rem] h-20 place-content-center">
+        <section className="pagination flex flex-col place-content-center">
+            <span className="text-sm text-black">
+                Showing <span className="font-semibold text-black">1</span> to <span className="font-semibold text-black">6</span>
+            </span>
+            <div
+                className="flex"
+            // className="flex w-[7rem] h-20"
+            >
                 {/* <!-- Help text --> */}
-                <span class="text-sm text-black">
-                    Showing <span class="font-semibold text-black">1</span> to <span class="font-semibold text-black">6</span>
-                </span>
                 {/* <!-- Buttons --> */}
-                <div class="inline-flex mt-2 xs:mt-0">
-                    <button class="flex items-center justify-center px-3 h-8 text-sm font-medium text-white bg-gray-800 rounded-l hover:bg-[#A6CF98] dark:text-gray-400 dark:hover:text-white">Prev</button>
-                    <button class="flex items-center justify-center px-3 h-8 text-sm font-medium text-white bg-gray-800 border-0 border-l rounded-r hover:bg-[#A6CF98] dark:text-gray-400 dark:hover:text-white">Next</button>
-                </div>
+                <button className="flex items-center justify-center px-3 h-8 text-sm font-medium text-white bg-gray-800 rounded-l hover:bg-[#A6CF98] dark:text-gray-400 dark:hover:text-white">Prev</button>
+                <button className="flex items-center justify-center px-3 h-8 text-sm font-medium text-white bg-gray-800 border-0 border-l rounded-r hover:bg-[#A6CF98] dark:text-gray-400 dark:hover:text-white">Next</button>
+                {/* <div className="inline-flex mt-2 xs:mt-0">
+                </div> */}
             </div>
         </section>
     )
