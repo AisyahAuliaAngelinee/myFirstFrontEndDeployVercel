ba1dca7bee6cee6777c55a41952f3356b81983d1
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2023-11-09 23:51:41 +0700

    feat: detail error

--- resto-public/src/components/DetailCuisine.jsx
+++
@@ -1,9 +0,0 @@
-const DetailCuisine = () => {
-	return (
-		<>
-			<h1>DETAIL {id}</h1>
-		</>
-	);
-};
-
-export default DetailCuisine;
--- resto-public/src/views/Detail.jsx
+++ resto-public/src/views/Detail.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
@@ -5,7 +6,7 @@ const Detail = () => {
 	const { id } = useParams();
 
 	const [data, setData] = useState([]);
-	const [isloading, setIsLoading] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState("");
 
 	useEffect(() => {
@@ -13,15 +14,50 @@ const Detail = () => {
 			setIsLoading("Loading");
 			setError({ error });
 			try {
+				const { data } = await axios("http://localhost:3000/cuisine" + id);
+				// console.log(data, "<<<<<<");
+				
+				setData(data);
 			} catch (error) {
 				console.log(error);
+				setError(error);
 			} finally {
+				setIsLoading(false);
 			}
 		};
+		fetchData();
 	});
+
+	if (isLoading) return <p>Loading</p>;
+	if (error) return <p>{error.message}</p>;
+
 	return (
 		<>
-			<h1>Detail {id}</h1>
+			{/* <h1>Detail {id}</h1> */}
+			<section class="detail">
+				<div class="detail-cuisine">
+					<div class="detail-image">
+						<img src="https://dcostseafood.id/wp-content/uploads/2017/12/Nasi-Goreng-seafood-2.jpg" alt="" />
+					</div>
+					<div class="detail-description">
+						{/* <span class="category">HEAVY MEAL</span> */}
+						<h3>Nasi Goreng Kasablangka</h3>
+						<span class="price">Rp. 198.000</span>
+						<p>
+							Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+							Praesentium aperiam minus cumque aut reprehenderit dolore
+							obcaecati non quo, cum eius magnam ducimus dolores itaque? Alias
+							odio repellat iste dolores iure!
+						</p>
+						<button class="text-white px-4 py-1 rounded text-sm font-medium bg-gray-800 border-0 border-l hover:bg-[#16a34a] dark:text-gray-400 dark:hover:text-white">
+							Edit
+						</button>
+						<button class="text-white px-2 py-1 rounded text-sm font-medium bg-gray-800 border-0 border-l hover:bg-[#dc2626] dark:text-gray-400 dark:hover:text-white">
+							Delete
+						</button>
+					</div>
+				</div>
+			</section>
 		</>
 	);
 };
