19fa6289f3cf8d52489066296e24588e6fb87a7d
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2023-11-10 16:42:50 +0700

    feat: cms

--- resto-app/src/App.jsx
+++ resto-app/src/App.jsx
@@ -1,41 +1,10 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-import "./App.css";
-import Header from "../components/Navbar";
-import Login from "../components/Login";
-import Form from "../components/Form";
-import MainTable from "../components/mainTable";
-import SupportTable from "../components/SupportTable";
-import Register from "../components/Register";
-
+import {RouterProvider} from "react-router-dom"
 function App() {
 	return (
 		<>
-			{/* HEADER START */}
-			<Header />
-			{/* HEADER END */}
-
-			{/* <!-- LOGIN FORM START --> */}
-			<Login />
-			{/* <!-- LOGIN FORM END --> */}
-
-			{/* <!-- LIST ENTITAS UTAMA START --> */}
-			<MainTable />
-			{/* <!-- LIST ENTITAS UTAMA END --> */}
-
-			{/* <!-- FORM CREATE CUISINE START --> */}
-			<Form />
-			{/* <!-- FORM CREATE CUISINE END --> */}
-
-			{/* <!-- LIST ENTITAS SUPPORT START --> */}
-			<SupportTable />
-			{/* <!-- LIST ENTITAS SUPPORT END --> */}
-
-			{/* <!-- REGISTER FORM START --> */}
-			<Register />
-			{/* <!-- REGISTER FORM END --> */}
+			<RouterProvider router={} />
 		</>
 	);
-}
+};
 
 export default App;
