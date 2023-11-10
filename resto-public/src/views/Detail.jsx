1d2378b64394d42a8eedb2085f4f8a381a89aad4
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2023-11-10 08:34:59 +0700

    feat: fixing detail

--- resto-public/src/views/Detail.jsx
+++ resto-public/src/views/Detail.jsx
@@ -1,47 +1,47 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const Detail = () => {
-	const { id } = useParams();
+	const { name } = useParams();
+	console.log(name, "<<<<<<<");
 
-	const [data, setData] = useState([]);
-	const [isLoading, setIsLoading] = useState(false);
-	const [error, setError] = useState("");
+	// const [data, setData] = useState([]);
+	// const [isLoading, setIsLoading] = useState(false);
+	// const [error, setError] = useState("");
 
-	useEffect(() => {
-		const fetchData = async () => {
-			setIsLoading("Loading");
-			setError({ error });
-			try {
-				const { data } = await axios("http://localhost:3000/cuisine" + id);
-				// console.log(data, "<<<<<<");
+	// useEffect(() => {
+	// 	const fetchData = async () => {
+	// 		setIsLoading("Loading");
+	// 		setError({ error });
+	// 		try {
+	// 			setIsLoading(true)
+	// 			const { data } = await cuisineApi.get(`detail/${name}`)
+	// 			// console.log(data, "<<<<<<");
 				
-				setData(data);
-			} catch (error) {
-				console.log(error);
-				setError(error);
-			} finally {
-				setIsLoading(false);
-			}
-		};
-		fetchData();
-	});
+	// 			setData(data);
+	// 		} catch (error) {
+	// 			console.log(error);
+	// 			setError(error);
+	// 		} finally {
+	// 			setIsLoading(false);
+	// 		}
+	// 	};
+	// 	fetchData();
+	// });
 
-	if (isLoading) return <p>Loading</p>;
-	if (error) return <p>{error.message}</p>;
+	// if (isLoading) return <p>Loading</p>;
+	// if (error) return <p>{error.message}</p>;
 
 	return (
 		<>
-			{/* <h1>Detail {id}</h1> */}
-			<section class="detail">
+			<h1>Detail {name}</h1>
+			{/* <section class="detail">
 				<div class="detail-cuisine">
 					<div class="detail-image">
 						<img src="https://dcostseafood.id/wp-content/uploads/2017/12/Nasi-Goreng-seafood-2.jpg" alt="" />
 					</div>
-					<div class="detail-description">
+					<div class="detail-description"> */}
 						{/* <span class="category">HEAVY MEAL</span> */}
-						<h3>Nasi Goreng Kasablangka</h3>
+						{/* <h3>Nasi Goreng Kasablangka</h3>
 						<span class="price">Rp. 198.000</span>
 						<p>
 							Lorem ipsum dolor sit, amet consectetur adipisicing elit.
@@ -57,7 +57,7 @@ const Detail = () => {
 						</button>
 					</div>
 				</div>
-			</section>
+			</section> */}
 		</>
 	);
 };
